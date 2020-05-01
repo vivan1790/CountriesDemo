7b777ce6bb3d2963601a78abd56bf415d32a4590
Vivan Verma <vivan1790@gmail.com>
2020-05-02 00:56:44 +0530

    4.3 Updating MainActivity layout

---
+++ app/src/main/java/com/sample/worldcountries/view/MainActivity.java
@@ -0,0 +1,32 @@
+package com.sample.worldcountries.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.sample.worldcountries.R;
+import com.sample.worldcountries.viewmodel.CountriesViewModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.countriesList) RecyclerView countriesList;
+    @BindView(R.id.errorMessage) TextView errorMessage;
+    @BindView(R.id.progressBar) ProgressBar progressBar;
+
+    private CountriesViewModel listViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        countriesList.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
