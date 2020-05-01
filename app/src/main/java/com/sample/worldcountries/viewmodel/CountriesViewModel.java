10bf4a49993b82325015ea6b66c718559cae28da
Vivan Verma <vivan1790@gmail.com>
2020-05-02 00:49:06 +0530

    3.2 Adding ViewModel for list of countries

---
+++ app/src/main/java/com/sample/worldcountries/viewmodel/CountriesViewModel.java
@@ -0,0 +1,45 @@
+package com.sample.worldcountries.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.sample.worldcountries.model.Country;
+import com.sample.worldcountries.networking.CountriesService;
+
+import java.util.List;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CountriesViewModel extends ViewModel {
+
+    private MutableLiveData<List<Country>> countriesLiveData = new MutableLiveData<>();
+    private MutableLiveData<Boolean> loadingErrorLiveData = new MutableLiveData<>();
+
+    CountriesService countriesService = new CountriesService();
+
+    public void refreshList() {
+        countriesService.fetchCountries().enqueue(new Callback<List<Country>>() {
+            @Override
+            public void onResponse(Call<List<Country>> call, Response<List<Country>> response) {
+                countriesLiveData.postValue(response.body());
+                loadingErrorLiveData.postValue(false);
+            }
+
+            @Override
+            public void onFailure(Call<List<Country>> call, Throwable t) {
+                loadingErrorLiveData.postValue(true);
+            }
+        });
+    }
+
+    public MutableLiveData<List<Country>> getCountriesLiveData() {
+        return countriesLiveData;
+    }
+
+    public MutableLiveData<Boolean> getLoadingErrorLiveData() {
+        return loadingErrorLiveData;
+    }
+}
